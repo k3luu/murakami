d7aa6e6f055c276aba381114537d84a82a791545
Kathy Luu <kathyl@corp.doctor.com>
2019-12-02 14:34:13 -0800

    added faq content

--- src/pages/faq.js
+++ src/pages/faq.js
@@ -4,45 +4,92 @@ import styled from "@emotion/styled"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-export const Table = styled.table`
-  td:first-child {
-    width: 20px;
-  }
-`
-
 const FaqPage = () => (
   <Layout>
     <SEO title="Frequently Asked Questions" />
     <h1>Frequently Asked Questions</h1>
 
-    <Table>
-      <tbody>
-        <tr>
-          <td>1.</td>
-          <td>Where can we stay?</td>
-        </tr>
-        <tr>
-          <td></td>
-          <td>A list of hotels for guests will be added soon!</td>
-        </tr>
-        <tr>
-          <td>2.</td>
-          <td>Are children allowed?</td>
-        </tr>
-        <tr>
-          <td></td>
-          <td>No.</td>
-        </tr>
-        <tr>
-          <td>3.</td>
-          <td>Are dogs allowed?</td>
-        </tr>
-        <tr>
-          <td></td>
-          <td>Yes.</td>
-        </tr>
-      </tbody>
-    </Table>
+    <ol>
+      <li>
+        <div>When do I need to RSVP by?</div>
+        <div>Please RSVP by August 19, 2020.</div>
+      </li>
+      <li>
+        <div>Can I bring a date?</div>
+        <div>Please check with Jenny and Tyler about +1's.</div>
+      </li>
+      <li>
+        <div>Are kids welcome?</div>
+        <div>We have decided to make Saturday an adults-only event.</div>
+      </li>
+      <li>
+        <div>Where is the wedding?</div>
+        <div>Both the ceremony and reception will take place at Viansa Sonoma Winery located at 25200 Arnold Dr, Sonoma, CA 95476.</div>
+      </li>
+      <li>
+        <div>Is the wedding indoors or outdoors?</div>
+        <div>The ceremony and cocktail hour will hosted outdoors on the lawn. Dinner and dancing will be indoors in the paviliion.</div>
+      </li>
+      <li>
+        <div>What if I have dietary restrictions?</div>
+        <div>Please let Jenny or Tyler know if you have any dietary restrictions.</div>
+      </li>
+      <li>
+        <div>Is this wedding open bar?</div>
+        <div>The winery only has a permit to serve beer and wine, so hard alcohol is not allowed onsite. There will be a variety of wines from Viansa Winery and selection of beers. Plenty to ensure a fun-filled evening :)</div>
+      </li>
+      <li>
+        <div>What should I wear?</div>
+        <div>Cocktail attire for the wedding ceremony and reception on Saturday. Casual is fine if you're joining us for the cocktail reception on Friday. </div>
+      </li>
+      <li>
+        <div>Are there Ubers available?</div>
+        <div>Ubers (and other Rideshare apps, I guess) are available throughout Sonoma and Napa, but ETA's can take a bit longer so please plan accordingly if you're taking one to the wedding.</div>
+      </li>
+      <li>
+        <div>I am coming from out of town. Where should I stay?</div>
+        <div>Check our the travel page for more information on lodging.</div>
+      </li>
+      <li>
+        <div>What if we can't make it to Sonoma?</div>
+        <div>We understand it's a trek for a lot of people. We promise we won't hold it against you if you're not able to make it. </div>
+      </li>
+      <li>
+        <div>Will there be any activities happening that I need to know about?</div>
+        <div>Check out the Schedule page for details on the weekend's festivities. We also have some recommendations for you if you plan on getting to Sonoma early or are staying late under the Travel page.</div>
+      </li>
+      <li>
+        <div>Is it okay to take pictures with our phones and cameras during the wedding?</div>
+        <div>We ask that you do not take any photography or video during the ceremony. We will have professional videography and photography and don't want your beautiful faces covered by a camera or phones!</div>
+      </li>
+      <li>
+        <div>Is there a wedding hashtag that we should use?</div>
+        <div>Alright, if anyone knows Tyler, you know he takes a lot of pride in his witty puns. Here were some of the contenders.
+
+#JT2020WeddingExperience as long as you have your #LuuandTy
+#Marriedkami
+#JennyMarriedKami
+#JennyTysTheKnot
+#JLuu&TyMuu
+#Jluu&TyToo020
+#TyedtoLuu
+
+Ultimately, we're going with #
+Check out the Photos page for your tagged photos!</div>
+      </li>
+      <li>
+        <div>What should/could I do between the ceremony and the reception?</div>
+        <div>There will be a cocktail hour between the ceremony and the reception. Viansa is an amazing venue and there is plenty of room for guests to explore and enjoy the breathtaking views of Sonoma.</div>
+      </li>
+      <li>
+        <div>Where are we registered?</div>
+        <div>Please check back.</div>
+      </li>
+      <li>
+        <div>Who built this beautiful website?</div>
+        <div>Courtesy of Kathy Luu, Software Engineer & Maid of Honor.</div>
+      </li>
+    </ol>
   </Layout>
 )
 
