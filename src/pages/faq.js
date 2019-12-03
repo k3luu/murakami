c63ba3bd2118903d3e8ac86649e4b11debff7505
Kathy Luu <kathyluu820@gmail.com>
2019-12-02 19:03:59 -0800

    styled the faq section

--- src/pages/faq.js
+++ src/pages/faq.js
@@ -3,93 +3,162 @@ import styled from "@emotion/styled"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import * as palette from "../styles/palette"
+
+const List = styled.ul`
+  list-style: none;
+
+  li {
+    border-bottom: 1px solid ${palette.secondaryColor};
+    padding: 30px 0;
+  }
+
+  li > div:first-child {
+    color: ${palette.primaryColor};
+    font-size: 18px;
+    font-weight: bold;
+    padding-bottom: 20px;
+  }
+
+  li > p {
+  }
+`
 
 const FaqPage = () => (
   <Layout>
     <SEO title="Frequently Asked Questions" />
     <h1>Frequently Asked Questions</h1>
 
-    <ol>
+    <List>
       <li>
         <div>When do I need to RSVP by?</div>
-        <div>Please RSVP by August 19, 2020.</div>
+        <p>Please RSVP by August 19, 2020.</p>
       </li>
       <li>
         <div>Can I bring a date?</div>
-        <div>Please check with Jenny and Tyler about +1's.</div>
+        <p>Please check with Jenny and Tyler about +1's.</p>
       </li>
       <li>
         <div>Are kids welcome?</div>
-        <div>We have decided to make Saturday an adults-only event.</div>
+        <p>We have decided to make Saturday an adults-only event.</p>
       </li>
       <li>
         <div>Where is the wedding?</div>
-        <div>Both the ceremony and reception will take place at Viansa Sonoma Winery located at 25200 Arnold Dr, Sonoma, CA 95476.</div>
+        <p>
+          Both the ceremony and reception will take place at Viansa Sonoma
+          Winery located at 25200 Arnold Dr, Sonoma, CA 95476.
+        </p>
       </li>
       <li>
         <div>Is the wedding indoors or outdoors?</div>
-        <div>The ceremony and cocktail hour will hosted outdoors on the lawn. Dinner and dancing will be indoors in the paviliion.</div>
+        <p>
+          The ceremony and cocktail hour will hosted outdoors on the lawn.
+          Dinner and dancing will be indoors in the paviliion.
+        </p>
       </li>
       <li>
         <div>What if I have dietary restrictions?</div>
-        <div>Please let Jenny or Tyler know if you have any dietary restrictions.</div>
+        <p>
+          Please let Jenny or Tyler know if you have any dietary restrictions.
+        </p>
       </li>
       <li>
         <div>Is this wedding open bar?</div>
-        <div>The winery only has a permit to serve beer and wine, so hard alcohol is not allowed onsite. There will be a variety of wines from Viansa Winery and selection of beers. Plenty to ensure a fun-filled evening :)</div>
+        <p>
+          The winery only has a permit to serve beer and wine, so hard alcohol
+          is not allowed onsite. There will be a variety of wines from Viansa
+          Winery and selection of beers. Plenty to ensure a fun-filled evening
+          :)
+        </p>
       </li>
       <li>
         <div>What should I wear?</div>
-        <div>Cocktail attire for the wedding ceremony and reception on Saturday. Casual is fine if you're joining us for the cocktail reception on Friday. </div>
+        <p>
+          Cocktail attire for the wedding ceremony and reception on Saturday.
+          Casual is fine if you're joining us for the cocktail reception on
+          Friday.
+        </p>
       </li>
       <li>
         <div>Are there Ubers available?</div>
-        <div>Ubers (and other Rideshare apps, I guess) are available throughout Sonoma and Napa, but ETA's can take a bit longer so please plan accordingly if you're taking one to the wedding.</div>
+        <p>
+          Ubers (and other Rideshare apps, I guess) are available throughout
+          Sonoma and Napa, but ETA's can take a bit longer so please plan
+          accordingly if you're taking one to the wedding.
+        </p>
       </li>
       <li>
         <div>I am coming from out of town. Where should I stay?</div>
-        <div>Check our the travel page for more information on lodging.</div>
+        <p>Check our the travel page for more information on lodging.</p>
       </li>
       <li>
         <div>What if we can't make it to Sonoma?</div>
-        <div>We understand it's a trek for a lot of people. We promise we won't hold it against you if you're not able to make it. </div>
+        <p>
+          We understand it's a trek for a lot of people. We promise we won't
+          hold it against you if you're not able to make it.{" "}
+        </p>
       </li>
       <li>
-        <div>Will there be any activities happening that I need to know about?</div>
-        <div>Check out the Schedule page for details on the weekend's festivities. We also have some recommendations for you if you plan on getting to Sonoma early or are staying late under the Travel page.</div>
+        <div>
+          Will there be any activities happening that I need to know about?
+        </div>
+        <p>
+          Check out the Schedule page for details on the weekend's festivities.
+          We also have some recommendations for you if you plan on getting to
+          Sonoma early or are staying late under the Travel page.
+        </p>
       </li>
       <li>
-        <div>Is it okay to take pictures with our phones and cameras during the wedding?</div>
-        <div>We ask that you do not take any photography or video during the ceremony. We will have professional videography and photography and don't want your beautiful faces covered by a camera or phones!</div>
+        <div>
+          Is it okay to take pictures with our phones and cameras during the
+          wedding?
+        </div>
+        <p>
+          We ask that you do not take any photography or video during the
+          ceremony. We will have professional videography and photography and
+          don't want your beautiful faces covered by a camera or phones!
+        </p>
       </li>
       <li>
         <div>Is there a wedding hashtag that we should use?</div>
-        <div>Alright, if anyone knows Tyler, you know he takes a lot of pride in his witty puns. Here were some of the contenders.
-
-#JT2020WeddingExperience as long as you have your #LuuandTy
-#Marriedkami
-#JennyMarriedKami
-#JennyTysTheKnot
-#JLuu&TyMuu
-#Jluu&TyToo020
-#TyedtoLuu
-
-Ultimately, we're going with #
-Check out the Photos page for your tagged photos!</div>
+        <p>
+          Alright, if anyone knows Tyler, you know he takes a lot of pride in
+          his witty puns. Here were some of the contenders.
+          <br />
+          <br />
+          <div>#JT2020WeddingExperience as long as you have your #LuuandTy</div>
+          <div>#Marriedkami</div>
+          <div>#JennyMarriedKami</div>
+          <div>#JennyTysTheKnot</div>
+          <div>#JLuu&TyMuu</div>
+          <div>#Jluu&TyToo020</div>
+          <div>#TyedtoLuu</div>
+          <br />
+          <br />
+          Ultimately, we're going with #
+          <br />
+          Check out the Photos page for your tagged photos!
+        </p>
       </li>
       <li>
-        <div>What should/could I do between the ceremony and the reception?</div>
-        <div>There will be a cocktail hour between the ceremony and the reception. Viansa is an amazing venue and there is plenty of room for guests to explore and enjoy the breathtaking views of Sonoma.</div>
+        <div>
+          What should/could I do between the ceremony and the reception?
+        </div>
+        <p>
+          There will be a cocktail hour between the ceremony and the reception.
+          Viansa is an amazing venue and there is plenty of room for guests to
+          explore and enjoy the breathtaking views of Sonoma.
+        </p>
       </li>
       <li>
         <div>Where are we registered?</div>
-        <div>Please check back.</div>
+        <p>Please check back.</p>
       </li>
       <li>
         <div>Who built this beautiful website?</div>
-        <div>Courtesy of Kathy Luu, Software Engineer & Maid of Honor.</div>
+        <p>Courtesy of Kathy Luu, Software Engineer & Maid of Honor.</p>
       </li>
-    </ol>
+    </List>
   </Layout>
 )
 
--- src/styles/palette.js
+++ src/styles/palette.js
@@ -1,4 +1,5 @@
 export const primaryColor = "#74a3b7"
-export const gray = "#6b7d83";
-export const darkGrey = "#212b2f";
+export const secondaryColor = "#f2eee2"
+export const gray = "#6b7d83"
+export const darkGrey = "#212b2f"
 export const white = "#fff"
